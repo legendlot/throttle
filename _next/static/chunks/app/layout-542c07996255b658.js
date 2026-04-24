74f825dd3cf99d6f78aee9893601b43bd088d397
legendlot <legendlot@users.noreply.github.com>
2026-04-24 18:21:59 +0000

    deploy: 109ca472674ddea7eb32e427e4a883914060416c

---
+++ _next/static/chunks/app/layout-542c07996255b658.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2545:function(e,n,i){Promise.resolve().then(i.t.bind(i,2625,23)),Promise.resolve().then(i.bind(i,7133)),Promise.resolve().then(i.bind(i,2486))},2625:function(){}},function(e){e.O(0,[387,93,403,486,293,528,744],function(){return e(e.s=2545)}),_N_E=e.O()}]);
